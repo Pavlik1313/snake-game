17dbc69eee2de833d021bf628090806b603ae8ad
38097 <vlad_pavlyuk_@ukr.net>
2022-12-06 14:33:21 +0200

    .

--- src/components/startForm.js
+++ src/components/startForm.js
@@ -5,7 +5,8 @@ export default function startForm(){
                 <h1>Snake game!)</h1>
                 <h4>Please enter your username</h4>
                 <input className='username-input' placeholder='Username'/>
-                <button className='start-button'>Start!</button>
+                <button className='start-button'
+                onClick={()=>{document.getElementsByClassName('form-background')[0].remove()}}>Start!</button>
             </div>
         </div>)
 
